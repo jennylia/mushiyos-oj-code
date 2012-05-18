60eaf27912d2836d5b8752df61111a853b566bc5
mushiyo@gmail.com <mushiyo@gmail.com@c707a46e-63cc-6533-e666-19cc5fc6cace>
2012-05-18 15:01:01 +0000

    add frequently used data structures & algorithms

---
+++ trunk/Data Structures & Algorithms/DsAndAlgo/AbstractTree.java
@@ -0,0 +1,26 @@
+package DsAndAlgo;
+import java.util.Iterator;
+
+public abstract class AbstractTree<E extends Comparable<E>> implements Tree<E> {
+	//Inorder traversal from the root
+    public void inorder(){
+    }
+    
+    //Postorder traversal from the root
+    public void postorder(){
+    }
+    
+    //Preorder traversal from the root
+    public void preorder(){
+    }
+
+  //return true if the tree is empty
+    public boolean isEmpty(){
+    	return getSize() == 0;
+    }
+    
+    //return an iterator to traverse elements in the tree
+    public Iterator iterator(){
+    	return null;
+    }
+}
