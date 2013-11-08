1f4cfb6fef861c4faa4dccae6bbb67b0e8ffa686
mushiyo@gmail.com <mushiyo@gmail.com@c707a46e-63cc-6533-e666-19cc5fc6cace>
2013-11-08 16:36:56 +0000

    [UVa] 12210 AC

---
+++ branches/UVa/u12210.cpp
@@ -0,0 +1,44 @@
+/*Filename: u12210.cpp
+ *Author: Mushiyo
+ */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	int caseNum = 1;
+	int B = 0, S = 0;
+
+    while(cin >> B >> S){
+    	if(B == 0 && S == 0){
+    		break;
+    	}
+
+        int bachelor[B];
+        for(int i = 0; i < B; ++i){
+        	cin >> bachelor[i];
+        }
+
+
+        int spinster[S];
+        for(int i = 0; i < S; ++i){
+        	cin >> spinster[i];
+        }
+
+        cout << "Case " << caseNum << ": ";
+        if(B <= S){
+        	cout << 0 << endl;
+        }
+        else{
+        	cout << B - S << " ";
+        	sort(bachelor, bachelor + B, greater<int>());
+        	cout << bachelor[B - 1] << endl;
+        }
+        
+        ++caseNum;
+    }
+
+	return 0;
+}
