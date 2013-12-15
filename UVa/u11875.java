26a6e59a218aab8a44999298591750b23c51c590
mushiyo@gmail.com <mushiyo@gmail.com@c707a46e-63cc-6533-e666-19cc5fc6cace>
2013-12-15 12:21:30 +0000

    UVa11875 AC in Java

---
+++ UVa/u11875.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class u11875 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		while (input.hasNext()) {
+			int T = input.nextInt();
+
+			for (int caseNum = 1; caseNum <= T; ++caseNum) {
+				int N = input.nextInt();
+
+				int halfN = (N / 2);
+
+				int captainAge = 0;
+				for (int i = 0; i < N; ++i) {
+					int age = input.nextInt();
+
+					if (i == halfN) {
+						captainAge = age;
+					}
+				}
+
+				System.out.printf("Case %d: %d\n", caseNum, captainAge);
+			}
+
+		}
+	}
+}
