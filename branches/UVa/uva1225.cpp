c3e5fcea58b8ffbdfa44128dd05af6ef11bd371e
mushiyo@gmail.com <mushiyo@gmail.com@c707a46e-63cc-6533-e666-19cc5fc6cace>
2013-12-16 15:09:30 +0000

    uva1225 AC in C++

---
+++ branches/UVa/uva1225.cpp
@@ -0,0 +1,41 @@
+/*Filename: uva1225.cpp
+ *Author: Mushiyo
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+	char numStr[6];
+	int dataSetNum = 0;
+	const int DIGITS = 10;
+
+    while(cin >> dataSetNum){
+        while(dataSetNum--){
+        	int N = 0;
+        	cin >> N;
+
+        	int countDigit[DIGITS];
+        	memset(countDigit, 0, sizeof(countDigit));
+        	for(int num = 1; num <= N; ++num){
+        		sprintf(numStr, "%d", num);
+
+        		int numStrLen = strlen(numStr);
+        		for(int i = 0; i < numStrLen; ++i){
+        			++countDigit[numStr[i] - '0'];
+        		}
+        	}
+
+        	cout << countDigit[0];
+        	for(int i = 1; i < DIGITS; ++i){
+        		cout << " " << countDigit[i];
+        	}
+        	cout << endl;
+        }
+    }
+
+	return 0;
+}
