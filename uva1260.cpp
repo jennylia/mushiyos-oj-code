c61e238d717a782c5028aa99b517d7f8fd45b162
mushiyo@gmail.com <mushiyo@gmail.com@c707a46e-63cc-6533-e666-19cc5fc6cace>
2013-12-19 13:06:25 +0000

    UVa1260 AC in C++

---
+++ uva1260.cpp
@@ -0,0 +1,49 @@
+/*Filename: uva1260.cpp
+ *Author: Mushiyo
+ */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+	int T = 0;
+
+	while(cin >> T){
+		while(T--){
+			int n = 0;
+			cin >> n;
+
+			int A[n];
+			memset(A, 0, sizeof(A));
+
+			const int LEN_B = n - 1;
+			int B[LEN_B];
+			memset(B, 0, sizeof(B));
+
+			cin >> A[0];
+			for(int i = 1; i < n; ++i){
+				cin >> A[i];
+				int countNongreater = 0;
+
+				for(int j = 0; j < i; ++j){
+					if(A[j] <= A[i]){
+						++countNongreater;
+					}
+				}
+
+				B[i - 1] = countNongreater;
+			}
+
+			int sumB = 0;
+			for(int i = 0; i < LEN_B; ++i){
+				sumB += B[i];
+			}
+
+			cout << sumB << endl;
+		}
+	}
+
+	return 0;
+}
