874ea8221c771d9efb5e30531d39702a77c955fa
mushiyo@gmail.com <mushiyo@gmail.com@c707a46e-63cc-6533-e666-19cc5fc6cace>
2014-08-13 16:34:47 +0000

    rename package and add some new library

---
+++ trunk/DataStructures&Algorithms/math/Combinatorics.java
@@ -0,0 +1,36 @@
+/* Filename: Combinatorics.java
+ * Author: Mushiyo
+ */
+package math;
+
+public class Combinatorics {
+	public static long factorial(int n) {
+		int nFactorial = 1;
+
+		for (int i = 2; i <= n; ++i) {
+			nFactorial *= i;
+		}
+
+		return nFactorial;
+	}
+
+	public static long permutation(int n, int m) {
+		int p = 1;
+
+		for (int i = n - m + 1; i <= n; ++i) {
+			p *= i;
+		}
+
+		return p;
+	}
+
+	public static long combination(int n, int m) {
+		int c = 1;
+
+		for (int i = Math.max(m, n - m) + 1, j = 1; i <= n; ++i, ++j) {
+			c = (c * i) / j;
+		}
+
+		return c;
+	}
+}
