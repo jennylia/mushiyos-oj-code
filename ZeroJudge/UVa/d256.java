e8a9005299052c12f8f2ef2498a3addebceeab2f
mushiyo@gmail.com <mushiyo@gmail.com@c707a46e-63cc-6533-e666-19cc5fc6cace>
2014-09-03 12:59:23 +0000

    commit AC codes for some ZeroJudge problems

--- ZeroJudge/UVa/d256.java
+++ ZeroJudge/UVa/d256.java
@@ -1,5 +1,3 @@
-// RE
-
 /* Filename: d256.java
  * Author: Mushiyo
  */
@@ -11,22 +9,21 @@ public class d256 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		while (input.hasNext()) {
-			int T = input.nextInt();
+		int T = input.nextInt();
 
-			while (T > 0) {
-				int G = input.nextInt();
-				int L = input.nextInt();
+		while (T > 0) {
+			int G = input.nextInt();
+			int L = input.nextInt();
 
-				if (gcd(G, L) == G) {
-					System.out.println(G + " " + L);
-				} else {
-					System.out.println(-1);
-				}
-
-				--T;
+			if (gcd(G, L) == G) {
+				System.out.println(G + " " + L);
+			} else {
+				System.out.println(-1);
 			}
+
+			--T;
 		}
+
 	}
 
 	public static int gcd(int a, int b) {
